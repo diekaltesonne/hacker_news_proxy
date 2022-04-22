6a68230122e246745836544963afafd76949e0f5
Ilya Trifonov <iitrifonov@sberautotech.ru>
2022-04-22 03:28:02 +0300

    fix of version 1.0

--- src/parser.py
+++ src/parser.py
@@ -1,21 +1,20 @@
-from encodings import utf_8
 from bs4 import BeautifulSoup as bs
 import re
 
-blacklist = [
-    "meta",
-    "script",
-]
+blacklist = ["script", "noscript", "style", "code" "meta", "link"]
 
 
 def filter(text):
     return re.sub(r"\b([а-яёa-z-]{6})\b", r"\1™", text, flags=re.IGNORECASE)
 
 
-def parse_html(context_data):
-    soup = bs(context_data, "html5lib")
+def parse_html(context_data, url):
+    soup = bs(context_data, "lxml")
+    for a in soup.findAll("a", href=True):
+        a["href"] = a["href"].replace("https://news.ycombinator.com", url)
+
     for el in soup.find_all(text=True):
         text = el.string
         if text and False not in [el.find_parent(x) is None for x in blacklist]:
             el.replace_with(filter(text))
-    return soup.prettify()
+    return str(soup)
--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from parser import parse_html
 import requests
 import logging
@@ -7,27 +7,45 @@ app = Flask(__name__.split(".")[0])
 logging.basicConfig(level=logging.INFO)
 APPROVED_HOSTS = " https://news.ycombinator.com/"
 LOG = logging.getLogger("app.py")
+REQUEST_SESSION = requests.Session()
 
 
 @app.route("/", defaults={"url_path": ""}, methods=["GET"])
 @app.route("/<path:url_path>", methods=["GET"])
 def proxy(url_path):
+
     redirect_url = "%s%s" % (
         url_path,
         ("?" + request.query_string.decode("utf-8") if request.query_string else ""),
     )
-    url = "{}{}".format(APPROVED_HOSTS, redirect_url)
 
-    req = requests.Request(
-        method=request.method,
-        url=url,
-        headers=request.headers,
-        data=request.data,
-    )
+    url = "{}{}".format(APPROVED_HOSTS, redirect_url)
+    resp = REQUEST_SESSION.get(url, headers=request.headers)
     LOG.debug("%s %s with headers: %s", request.method, url, request.headers)
-    resp = requests.Session().send(req.prepare(), stream=True)
-    content = parse_html(resp.content)
-    return content
+
+    excluded_headers = [
+        "content-encoding",
+        "content-length",
+        "transfer-encoding",
+        "connection",
+    ]
+
+    headers = [
+        (name, value)
+        for (name, value) in resp.headers.items()
+        if name.lower() not in excluded_headers
+    ]
+    content = resp.content
+
+    if resp.headers.get("Content-Type", "").startswith("text/html"):
+        content = parse_html(resp.content, request.host_url.strip("/"))
+
+    return Response(
+        content,
+        content_type=resp.headers["Content-Type"],
+        headers=headers,
+        status=resp.status_code,
+    )
 
 
 if __name__ == "__main__":
